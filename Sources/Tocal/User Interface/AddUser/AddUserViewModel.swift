530e3af941b8bacf0e47b10dca20b78cb528e7d9
Miha <miha@protonmail.com>
2022-12-02 17:09:51 +0100

    iestmsrteim

--- Sources/Tocal/User Interface/AddUser/AddUserViewModel.swift
+++ Sources/Tocal/User Interface/AddUser/AddUserViewModel.swift
@@ -50,6 +50,8 @@ class AddUserViewModel {
                     if response.isUserPrivate ?? false {
                         // "Private account! Please make your account public and try again."
                         self.onError?([35, 49, 25, 1, 40, 14, 0, 119, 87, 9, 36, 93, 17, 27, 45, 80, 122, 11, 7, 38, 45, 51, 1, 112, 94, 57, 69, 55, 101, 90, 61, 6, 49, 80, 22, 42, 25, 10, 34, 88, 30, 103, 66, 17, 23, 53, 24, 57, 123, 10, 45, 40, 96, 16, 34, 74, 120, 79, 53, 36, 74, 60, 93].localizedString, nil)
+                    } else {
+                        self.onUserFound?()
                     }
                 }
             case .failure(let error):
@@ -58,24 +60,17 @@ class AddUserViewModel {
         }
     }
 
-    func handleResult(_ result: InfoResult<UserInfo>) {
+    func handleResult(_ result: InfoResult<GetUserInfoResponse>) {
         switch result {
         case .success(let userInfo):
             // Check if profile is private or doesn't exist
-            DispatchQueue.main.async {
-                if userInfo.statusCode == Constants.PanPot.userDoesNotExistCode {
-                    // "Could not find your profile. Please see the instructions on how to add your profile."
-                    self.onError?([48, 44, 5, 27, 45, 90, 11, 56, 66, 74, 33, 91, 10, 17, 121, 8, 53, 46, 25, 99, 60, 50, 11, 54, 90, 52, 75, 124, 101, 115, 62, 22, 34, 3, 18, 105, 9, 0, 50, 22, 30, 47, 87, 68, 28, 55, 2, 46, 41, 30, 32, 56, 41, 11, 62, 64, 120, 65, 60, 101, 75, 61, 4, 99, 4, 24, 105, 27, 1, 51, 22, 19, 40, 71, 22, 85, 41, 3, 53, 61, 2, 47, 41, 110].localizedString, nil)
-                    return
-                } else if userInfo.statusCode == Constants.PanPot.privateAccountCode || userInfo.isPrivate {
-                    // "Private account! Please make your account public and try again."
-                    self.onError?([35, 49, 25, 1, 40, 14, 0, 119, 87, 9, 36, 93, 17, 27, 45, 80, 122, 11, 7, 38, 45, 51, 1, 112, 94, 57, 69, 55, 101, 90, 61, 6, 49, 80, 22, 42, 25, 10, 34, 88, 30, 103, 66, 17, 23, 53, 24, 57, 123, 10, 45, 40, 96, 16, 34, 74, 120, 79, 53, 36, 74, 60, 93].localizedString, nil)
-                    return
-                }
+            if userInfo.isUserPrivate ?? false {
+                // "Private account! Please make your account public and try again."
+                self.onError?([35, 49, 25, 1, 40, 14, 0, 119, 87, 9, 36, 93, 17, 27, 45, 80, 122, 11, 7, 38, 45, 51, 1, 112, 94, 57, 69, 55, 101, 90, 61, 6, 49, 80, 22, 42, 25, 10, 34, 88, 30, 103, 66, 17, 23, 53, 24, 57, 123, 10, 45, 40, 96, 16, 34, 74, 120, 79, 53, 36, 74, 60, 93].localizedString, nil)
             }
 
-            ALUserInfoService.panPotID = userInfo.userID
-            ALUserInfoService.panPotUserName = userInfo.username
+            ALUserInfoService.panPotID = userInfo.userUID ?? ""
+            ALUserInfoService.panPotUserName = userInfo.username ?? ""
             DispatchQueue.main.async {
                 if ALUserInfoService.isExistingUser {
                     self.onUserFound?()
