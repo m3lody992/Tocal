9abd43eba5bb02ff13c37fb745d025263173ec0c
m3lody992 <102649255+m3lody992@users.noreply.github.com>
2022-12-14 18:55:44 +0100

    syltnamnk

--- Sources/Tocal/Tocal/User Interface/Flowers/FlowerAstersVC.swift
+++ Sources/Tocal/Tocal/User Interface/Flowers/FlowerAstersVC.swift
@@ -82,6 +82,8 @@ class FlowerAsterViewController: kBRLNmf7mGhhxUj2Oou351YWEX8tW5mR {
         spinner1.startAnimating()
         spinner2.startAnimating()
         spinner3.startAnimating()
+        flowersLabel.text = nil
+        floweringsLabel = nil
         let userInfoModel = GetUserInfo(link: OL9vSPcigWst6xJKRSiVAnWOXScnaZ4P.TZjggTeWPL0sALTcnFedRwaHwsV5a3eL.lowercased())
         var routerEndpoint = MorrisRouter(endpoint: .getUserInfo)
         routerEndpoint.WijWIVRw1wa2lfKi4voAIXRVZd1I68eS(userInfoModel)
